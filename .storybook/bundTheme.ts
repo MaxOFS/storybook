00320bbd7deea2309f5880f786573a17bc53571a
Henking Max BFS <max.henking@bfs.admin.ch>
2026-02-26 15:37:09 +0100

    remove manager.js

--- .storybook/bundTheme.ts
+++
@@ -1,8 +0,0 @@
-import { create } from '@storybook/theming';
- 
-export default create({
-  base: 'light',
-  brandTitle: 'My custom Storybook',
-  brandImage: 'https://datavis-a.bfs.admin.ch/guidelines/assets/BundLogo.svg',
-  brandTarget: '_self',
-});
--- .storybook/manager.js
+++
@@ -1,6 +0,0 @@
-import { addons } from 'storybook/manager-api';
-import bundTheme from './bundTheme';
- 
-addons.setConfig({
-  theme: bundTheme,
-});
