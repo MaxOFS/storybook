fe6eee7c4485316158047b0587e336f041b1a846
Henking Max BFS <max.henking@bfs.admin.ch>
2026-03-10 08:52:40 +0100

    WIP on description and documentation

---
+++ src/stories/Visualisation Types/Diagrams/bubbleChart.stories.ts
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+
+import { DWComponent } from '../../../app/dw.component';
+
+const meta: Meta<DWComponent> = {
+    title: 'VisualisationTypes/bubbleChart',
+    component: DWComponent,
+    tags: ['!dev', '!autodocs'],
+    args: {
+    },
+  };
+
+export default meta;
+type Story = StoryObj<DWComponent>;
+
+export const description: Story = {
+  args: {
+    iframeUrl: "https://datavis-a.bfs.admin.ch/guidelines/assets/visualisationTypes/gr-bubble/index.html",
+  },
+}
+
+export const categorical: Story = {
+    args: {
+      iframeUrl: "https://datavis-a.bfs.admin.ch/guidelines/assets/visualisationTypes/gr-bubble-categorical/index.html",
+    },
+  }
+
+  
