d0f1321f0aa82d8266318dd1199d27ea1038e85e
Henking Max BFS <max.henking@bfs.admin.ch>
2026-04-28 08:56:55 +0200

    Maps content for review

---
+++ src/stories/Visualisation Types/Maps/qualitative.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+
+import { DWComponent } from '../../../app/dw.component';
+
+const meta: Meta<DWComponent> = {
+    title: 'VisualisationTypes/qualitative',
+    component: DWComponent,
+    tags: ['!dev', '!autodocs'],
+    args: {
+    },
+  };
+
+export default meta;
+type Story = StoryObj<DWComponent>;
+
+export const Description: Story = {
+  args: {
+    iframeUrl: "https://datavis.bfs.admin.ch/guidelines/visualisationTypes/maps/qualitative/index.html",
+  },
+}
